33e16687f598cae68bac72b4bddc8a12b4a2aa6f
Fatih Ceylan <fazilhan87@gmail.com>
2020-09-05 01:33:19 +0300

    ShopApp eklendi

---
+++ ShopApp.DataAccess/Concrete/EFCore/EfProductDal.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using ShopApp.DataAccess.Abstract;
+using ShopApp.DataAccess.Concrete.EfCore;
+using ShopApp.Entities;
+
+namespace ShopApp.DataAccess.Concrete.EFCore
+{
+    public class EfProductDal:EfCoreGenericRepository<Product,ShopContext>,IProductDal
+    {
+        public IEnumerable<Product> GetPopularProducts()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ShopApp.DataAccess/Concrete/MySql/MySqlProductDal.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShopApp.DataAccess.Concrete.MySql
+{
+    public class MySqlProductDal
+    {
+    }
+}
